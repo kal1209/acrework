7ed364fcf3eb09607e60043afb456534e29a6d11
bstar <lakal0809@outlook.com>
2022-11-28 19:09:29 -0500

    add locations into item board

---
+++ public/config/items.js
@@ -0,0 +1,15 @@
+const locations = [
+    {
+        name: 'bathroom',
+        url: 'bathroom.webp'
+    }, {
+        name: 'bedroom',
+        url: 'bedroom.webp'
+    }, {
+        name: 'homehall',
+        url: 'homehall.webp'
+    }, {
+        name: 'kitchen',
+        url: 'kitchen.webp'
+    }
+]
--- public/scenes/EditSceneScene.js
+++ public/scenes/EditSceneScene.js
@@ -7,6 +7,6 @@ var EditSceneScene = new Phaser.Class({
     preload: function () {
     },
     create: async function () {
-        this.add.dom(0, this.sys.game.canvas.height, 'div', `background-color: white; width: ${this.sys.game.canvas.width}px; height: 100px; font: 48px Arial`, 'Phaser').setOrigin(0, 1);
+        this.item_board = this.add.dom(this.sys.game.canvas.width / 2, this.sys.game.canvas.height, 'div', `background-color: white; width: ${this.sys.game.canvas.width / 2}px; height: 200px; padding: 10px`, '').setOrigin(0.5, 1);
     },
 });
