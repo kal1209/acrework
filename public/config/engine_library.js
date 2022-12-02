70ac6c4562d07bac9130c70e8fac11f6f565fec5
bstar <lakal0809@outlook.com>
2022-12-02 12:14:45 -0500

    Update engine regarding the tooltip

--- public/config/engine_library.js
+++ public/config/engine_library.js
@@ -1,10 +1,5 @@
 class Engine {
     constructor() {
-        this.depthList = {
-            location: 1,
-            widget: 2,
-        };
-
         this.items = [
             // locations
             {
@@ -44,6 +39,7 @@ class Engine {
                 url: 'stairs.webp',
                 depth: 3,
                 interactable: true,
+                title: 'Stairs',
                 draggable: true,
             },
             {
@@ -55,6 +51,7 @@ class Engine {
                 draggable: true,
             },
             {
+
                 name: 'closet',
                 type: 'widget',
                 url: 'closet.webp',
@@ -84,6 +81,7 @@ class Engine {
                 url: 'd1.webp',
                 depth: 2,
                 interactable: true,
+                title: 'Drawer',
                 draggable: true,
             },
             {
@@ -92,6 +90,7 @@ class Engine {
                 url: 'd3.webp',
                 depth: 2,
                 interactable: true,
+                title: 'Drawer',
                 draggable: true,
             },
             {
@@ -100,6 +99,7 @@ class Engine {
                 url: 'd2.webp',
                 depth: 2,
                 interactable: true,
+                title: 'Drawer',
                 draggable: true,
             },
             {
@@ -108,22 +108,49 @@ class Engine {
                 url: 's1.webp',
                 depth: 2,
                 interactable: true,
+                title: 'Shelf',
                 draggable: true,
             },
         ]
+
+        this.ui = [
+            {
+                name: 'frame_objname_gold',
+                url: 'frame_objname_gold.webp',
+                depth: 10,
+            }
+        ]
+
+        this.tooltip = undefined
     }
     getItem(name) {
         return this.items.find(e => e.name == name)
     }
+    getUI(name) {
+        return this.ui.find(e => e.name == name)
+    }
     addItem(scene, x, y, item) {
-        let container = scene.add.container(x, y)
-        container.add(scene.add.sprite(0, 0, item.name).setInteractive({
-            draggable: item.draggable,
+        let tmp = scene.add.sprite(x, y, item.name).setInteractive({
+            // draggable: item.draggable,
+            draggable: false,
             useHandCursor: item.interactable,
             pixelPerfect: true
-        }).setOrigin(0, 0).on('drag', function (pointer, dragX, dragY) {
+        }).setOrigin(0, 0).setDepth(item.depth).on('drag', function (pointer, dragX, dragY) {
             this.setPosition(dragX, dragY);
             console.log([item.name, dragX, dragY])
-        })).setDepth(item.depth)
+        }).on('pointerover', () => {
+            if (item.interactable) {
+                this.tooltip.setPosition(tmp.width / 2 + tmp.x, tmp.height / 2 + tmp.y - 100)
+                this.tooltip.list[1].setText(item.title)
+                this.tooltip.setAlpha(1)
+            }
+        }).on('pointerout', () => {
+            this.tooltip.setAlpha(0)
+        })
+    }
+    initTooltip(scene, infoBg) {
+        this.tooltip = scene.add.container(100, 100).setDepth(10).setAlpha(0)
+        this.tooltip.add(scene.add.sprite(0, 0, infoBg.name))
+        this.tooltip.add(scene.add.text(0, -5, '', { font: "bold 32px Arial", fill: "#fff" }).setOrigin(0.5))
     }
 }
--- public/scenes/test_scene.js
+++ public/scenes/test_scene.js
@@ -1,42 +1,44 @@
-let engine = new Engine();
-
 var TestScene = new Phaser.Class({
     Extends: Phaser.Scene,
     initialize: function () {
         Phaser.Scene.call(this, { "key": "TestScene" });
     },
-    init: function () { },
+    init: function () {
+        this.engine = new Engine();
+    },
     preload: function () {
-        for (const elem of engine.items) {
+        // load the item assets
+        for (const elem of this.engine.items) {
             this.load.image(`${elem.name}`, `../assets/images/${elem.type}/${elem.url}`)
+        }
+
+        // load the ui assets
+        for (const elem of this.engine.ui) {
+            this.load.image(`${elem.name}`, `../assets/images/ui/${elem.url}`)
         }
     },
     create: async function () {
+        this.engine.initTooltip(this, this.engine.getUI('frame_objname_gold'))
+
         // add location
-        engine.addItem(this, 0, 0, engine.getItem('kitchen'))
+        this.engine.addItem(this, 0, 0, this.engine.getItem('kitchen'))
 
         // add stairs
-        engine.addItem(this, 0, 0, engine.getItem('stairs'))
+        this.engine.addItem(this, 0, 0, this.engine.getItem('stairs'))
 
-        engine.addItem(this, 1425, 182, engine.getItem('closet'))
+        this.engine.addItem(this, 1425, 182, this.engine.getItem('closet'))
 
-        engine.addItem(this, 614, 260, engine.getItem('clock'))
+        this.engine.addItem(this, 614, 260, this.engine.getItem('clock'))
 
-        engine.addItem(this, 367, 225, engine.getItem('furniture_01'))
+        this.engine.addItem(this, 367, 225, this.engine.getItem('furniture_01'))
 
-        engine.addItem(this, 1028, 524, engine.getItem('furniture_03'))
+        this.engine.addItem(this, 1028, 524, this.engine.getItem('furniture_03'))
 
         // add drawers
-        engine.addItem(this, 1589, 682, engine.getItem('d1'))
-        engine.addItem(this, 1449, 728, engine.getItem('d3'))
-        engine.addItem(this, 448, 668, engine.getItem('d2'))
+        this.engine.addItem(this, 1589, 682, this.engine.getItem('d1'))
+        this.engine.addItem(this, 1449, 728, this.engine.getItem('d3'))
+        this.engine.addItem(this, 448, 668, this.engine.getItem('d2'))
 
-        engine.addItem(this, 1499, 258, engine.getItem('s1'))
-
-        // let widget = engine.getItem('closet');
-        // this.add.sprite(1500, 200, widget.name).setInteractive({
-        //     useHandCursor: widget.interactable,
-        //     pixelPerfect: true
-        // }).setOrigin(0, 0).setDepth(widget.depth)
+        this.engine.addItem(this, 1499, 258, this.engine.getItem('s1'))
     },
 });
